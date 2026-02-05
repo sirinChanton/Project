7f947b69cd4c2a23adcd179084cb947b9350c6e3
sirin chanton <164905606+sirinChanton@users.noreply.github.com>
2026-02-05 23:38:25 +0700

    commitPC

--- obj/Debug/net10.0/Project.AssemblyInfo.cs
+++ obj/Debug/net10.0/Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd33c958e908b56723cf3bd048368c90961e467a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4fadd41135711ad2bbc2e497d2ba6cf6ac7ab30")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
